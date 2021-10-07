3b3ff515c2b57d6c639426c839cb2c25a406f65e
CorneliusJose <josecornel07@gmail.com>
2021-10-07 13:49:14 +0700

    menambahkan file tugas ke repo

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Cornelius Jose, saya tingga di Rawamangun, Olahraga yang saya sukai adalah BuluTangkis";
+    }
+
+}
---
+++ application/views/v_about.php
@@ -0,0 +1,25 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Cornelius</li>
+        <li>Nama Belakang : Jose</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Ciledug Raya No. 168 Pesanggrang</li>
+    </ul>
+
+    <h4>Tempat lahir</h4>
+    <ul type="none">
+        <li>Cirebon</li>
+    </ul>
+
+    <h4>Olah raga Favorit</h4>
+    <ul type="square">
+        <li>BuluTangkis</li>
+        <li>Catur</li>
+    </ul>
+</section>
+
